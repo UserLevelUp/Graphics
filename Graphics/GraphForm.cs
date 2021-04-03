32585b04061dbf0dcfe0531ac4599724cfcde541
Marc Noon <marcnoon@userlevelup.com>
2021-04-03 16:20:59 -0700

    Document current functionality of each section of application

--- Graphics/GraphForm.cs
+++ Graphics/GraphForm.cs
@@ -26,6 +26,8 @@ namespace Graphics
 
         }
 
+        
+        // private variables ... make this more extensible and functional and testable
         private ReadOnlySequence<Point> spts = new ReadOnlySequence<Point>();
         private List<Point> pts = new List<Point>();
         private static Queue<Point> qpts = new System.Collections.Generic.Queue<Point>();
@@ -34,12 +36,19 @@ namespace Graphics
         private int minX = 0;
         private int minY = 0;
         private bool dequeing_flag = false;
-        private delegate void SafeCallDelegate();
 
 
+        // Green paint brush... TODO: build based on user selection
         private System.Drawing.SolidBrush myBrush3 = new System.Drawing.SolidBrush(System.Drawing.Color.Green);
 
+        // flags
         private Boolean runIt = false;
+
+        /// <summary>
+        /// panel4_Paint ... change name to GraphicPaint or Layer Paint etc...
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void panel4_Paint(object sender, PaintEventArgs e)
         {
                 if (this.runIt == true)
@@ -60,40 +69,55 @@ namespace Graphics
 
         private async Task performFormula()
         {
-            for (int i = 0; i < 1_500_000_000; i++)
+            // iterates over some plot or formula ... may need to change this to x, y, z etc...
+            // needs to use smarter iteration and plots by placing in a thread or threads
+            for (int x = 0; x < 1_500_000_000; x++)
             {
 
                 if (this.runIt == false)
                 {
                     break;
                 }
-                var ox = this.panel4.Width / 2;
-                var oy = this.panel4.Height /2;
 
-                var cos = Complex.Sqrt(Math.E * i * 0.0001 * Math.Cos(i * 0.001));
-                
-                var sin = Complex.Sqrt(Math.E * i * 0.0001 * Math.Sin(i * 0.001));
+                // the current offset uses the center of screen plus some number
+                var ox = this.panel4.Width / 2 + 1300;
+                var oy = this.panel4.Height /2 + 1300;
 
 
-                cos = new Complex(cos.Real, cos.Imaginary);
-                sin = new Complex(sin.Real, sin.Imaginary);
+                // zoom
+                var zoom = Math.E;
+
+                // Equations - not very extensible... move equations into a text string then figure out a way to generate these equations dynamically during a plot
+                var cos = Complex.Sqrt(zoom * x * 0.00001 * Math.Cos(x * 0.0001));
+                var sin = Complex.Sqrt(zoom * x * 0.00001 * Math.Sin(x * 0.0001));
+
+//                cos = new Complex(cos.Real, cos.Imaginary);
+//                sin = new Complex(sin.Real, sin.Imaginary);
+
+                // convert the plot during each iteration to a single point to be rendered
                 var pt = new Point(
                         ox + (cos.Imaginary == 0 ? (int)(cos.Real * 42) : (int)(cos.Imaginary * 42) * -1),
                         oy + (sin.Imaginary == 0 ? (int)(sin.Real * 42) : (int)(sin.Imaginary * 42) * -1));
-                //e.Graphics.DrawLine(new Pen(myBrush3),
-                //    pt,
-                //    new Point(pt.X + 1, pt.Y + 1)
-                //    );
+
+                // check that the plot has not already been plotted, otherwise skip or continue to next iteration
                 if (plotPoint.X == pt.X && plotPoint.Y == pt.Y)
                     continue;
-                plotPoint.X = pt.X + 0;
-                plotPoint.Y = pt.Y + 0;
-                //pts.Add(pt);
+
+                // save the current point after testing if it was already plotted last
+                // TODO: perhaps store points in a key value pair where the key is a plotted point... if it was already plotted for a given color then move on
+                plotPoint.X = pt.X;
+                plotPoint.Y = pt.Y;
+
+                // uses a Queue property to save a point 
+                // currently its global on this form ... should be more functional
                 qpts.Enqueue(new Point(pt.X, pt.Y));
 
-                if (i%1 == 0)
+                // after every single iteration that wasn't already plotted it will cause plot to invalidated and updated
+                if (x%1 == 0)
                 {
                     this.dequeing_flag = true;
+
+                    // move back to the windows form thread to perform windows form operations using the drawing area and the invoke command
                     this.panel4.Invoke(new Action(() =>
                         {
                                 panel4.Invalidate(new Rectangle(new Point(pt.X, pt.Y), new Size(2, 2)));
@@ -104,6 +128,11 @@ namespace Graphics
 
         }
 
+        /// <summary>
+        /// When graphing it fires an async task to plot a formula
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnGraph_Click(object sender, EventArgs e)
         {
             this.runIt = true;
